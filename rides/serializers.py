9ec841bc0c07ac7204d2013abb7f1c4fe8c55b52
Dhan Martin <dhan@segstream.com>
2025-10-31 19:57:22 +0800

    Rides List API: todays_ride_events field implementation

--- rides/serializers.py
+++ rides/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from rides.models import Ride, RideEvent
+from users.models import User
+
+
+class RideEventInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideEvent
+        fields = ['id', 'description', 'created_at']
+
 
 class RideSerializer(serializers.ModelSerializer):
+    rider = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(role='rider'))
+    driver = serializers.PrimaryKeyRelatedField(queryset=User.objects.filter(role='driver'))
+
+    todays_ride_events = RideEventInfoSerializer(many=True, source='ride_events', read_only=True)
+
     class Meta:
         model = Ride
         fields = "__all__"
--- rides/views.py
+++ rides/views.py
@@ -1,12 +1,30 @@
+from datetime import timedelta
+from django.db.models import Prefetch
+from django.utils import timezone
 from rest_framework import viewsets
+
 from rides.models import Ride, RideEvent
 from rides.serializers import RideSerializer, RideEventSerializer
 from users.permissions import IsAdminUserRole
 
+
 class RideViewSet(viewsets.ModelViewSet):
-    queryset = Ride.objects.all()
     serializer_class = RideSerializer
     permission_classes = [IsAdminUserRole]
+
+    def get_queryset(self):
+        since = timezone.now() - timedelta(hours=24)
+        qs = (
+            Ride.objects.select_related('rider', 'driver')
+            .prefetch_related(
+                Prefetch(
+                    'ride_events',
+                    queryset=RideEvent.objects.filter(created_at__gte=since),
+                )
+            )
+        )
+
+        return qs
 
 
 class RideEventViewSet(viewsets.ModelViewSet):
