9f550c6155db78a69143dd2077810cce44a603c3
Dhan Martin <dhan@segstream.com>
2025-10-31 18:39:54 +0800

    User is admin role permission to the API endpoints

--- rides/views.py
+++ rides/views.py
@@ -1,12 +1,15 @@
 from rest_framework import viewsets
 from rides.models import Ride, RideEvent
 from rides.serializers import RideSerializer, RideEventSerializer
+from users.permissions import IsAdminUserRole
 
 class RideViewSet(viewsets.ModelViewSet):
     queryset = Ride.objects.all()
     serializer_class = RideSerializer
+    permission_classes = [IsAdminUserRole]
 
 
 class RideEventViewSet(viewsets.ModelViewSet):
     queryset = RideEvent.objects.all()
     serializer_class = RideEventSerializer
+    permission_classes = [IsAdminUserRole]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from users.models import User
+from users.permissions import IsAdminUserRole
 from users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUserRole]
